ec0e2cb281e99c0f300b513e5d277cd4a12a5a4e
agvarelapru <agvarelapru@gmail.com>
2017-09-27 20:22:37 +0200

    Inicio Formulario boostrap
    Inicio de la paghina con formulario en boostrap

---
+++ recoger.php
@@ -0,0 +1,24 @@
+﻿<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	
+	<title></title>
+</head>
+<body>
+	<?php
+	$conexion=mysqli_connect("localhost","root","","2daw") or die("Problemas con la conexión");
+	
+	mysqli_query($conexion,"insert into formulario(nombre,apellidos,email,informacion) values 
+                       ('$_REQUEST[nombre]','$_REQUEST[apellidos]','$_REQUEST[email]','$_REQUEST[informacion]')")
+  or die("Problemas en el select".mysqli_error($conexion));
+
+mysqli_close($conexion);
+
+echo "El alumno fue dado de alta.";
+	
+	?>
+</body>
+</html>
+
+
