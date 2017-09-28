15ba33d14e42d808e72561ab8f132933fdf7d67e
agvarelapru <agvarelapru@gmail.com>
2017-09-28 12:58:48 +0200

    pagina identificacion
    pagina usuario  y contraseña

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">	
+<title>Formulario</title>
+<!-- CSS de Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="estilos.css" rel="stylesheet" media="screen">
+
+</head>
+
+
+<body>
+<div class="container" >   
+<h1>HOLA</h1>
+<div>
+</body>
+</html>
